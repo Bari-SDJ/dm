892187e952494106b6a25c6ad25a9f4d0b2f3078
mominul bari <bari_mominul@rubygroupe.jp>
2025-08-01 22:03:45 +0900

    Salauddin qoutation pdf

--- src/tcpdf/examples/tcpdf_quotation_11.php
+++ src/tcpdf/examples/tcpdf_quotation_11.php
@@ -518,7 +518,7 @@ body { margin-left: 0.59055118110236in; margin-right: 0.59055118110236in; margin
               <br>{$_POST['serialNoId']}<br><br>
               <br><b>{$_POST['0_2']}</b>&nbsp;&nbsp;&nbsp;&nbsp;<br />
               {$_POST['0_3']}&nbsp;&nbsp;&nbsp;&nbsp;<br>
-              TEL:{$_POST['0_4']} FAX:{$_POST['0_5']}&nbsp;&nbsp;&nbsp;&nbsp;<br>
+              MOB:{$_POST['0_4']} TEL&FAX:{$_POST['0_5']}&nbsp;&nbsp;&nbsp;&nbsp;<br>
               Mail:{$_POST['0_6']}&nbsp;&nbsp;&nbsp;&nbsp;<br>
               {$_POST['0_7']}&nbsp;&nbsp;&nbsp;&nbsp;
             </td>
